cf7fa1beb5f7d0b301cee16e0d64de8976909a2d
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 00:22:36 -0700

    (test) Add tests for add and contains

--- problems/graphs/tests/graph.test.js
+++ problems/graphs/tests/graph.test.js
@@ -6,7 +6,9 @@ const assert = require('chai').assert;
 describe('graph', () => {
   describe('graph constructor && methods', () => {
     it('should add a node to graph, and check with contains', () => {
-
+      const graph = new solution.Graph();
+      graph.addNode(1);
+      assert.equal(graph.contains(1), true);
     });
 
     it('should remove a node from the graph', () => {
