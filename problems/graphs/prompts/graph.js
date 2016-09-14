0f1dda55befd0dbbe4607573affa523f1539979b
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 01:06:42 -0700

    (feat) Add methods for Graph

--- problems/graphs/prompts/graph.js
+++ problems/graphs/prompts/graph.js
@@ -13,23 +13,45 @@ Graph.prototype.contains = function(node) {
 };
 
 Graph.prototype.removeNode = function(node) {
-
+  if (this.contains(node)) {
+    for (var edgeNode in this.nodes[node].edges) {
+      // handle edges in other existing nodes
+      this.removeEdge(edgeNode, node);
+    }
+    delete this.nodes[node];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    return;
+  }
 
+  return !!this.nodes[fromNode].edges[toNode];
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    return;
+  }
 
+  this.nodes[fromNode].edges[toNode] = toNode;
+  this.nodes[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    return;
+  }
 
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 Graph.prototype.forEachNode = function(callback) {
-
+  for (var eachNode in this.nodes) {
+    callback(eachNode);
+  }
 };
 
 module.exports = { Graph };
