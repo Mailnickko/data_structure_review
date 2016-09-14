ee0b069da2a5a97f7eb92b5363009aa6e846d482
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 16:45:09 -0700

    (feat) Add Queue and Stack test suites

--- problems/manifest.js
+++ problems/manifest.js
@@ -4,7 +4,9 @@
   './trial/tests/testingRun.test.js',
   './linkedList/tests/linkedList.test.js',
   './graphs/tests/graph.test.js',
-  './hashTables/tests/hashTable.test.js'
+  './hashTables/tests/hashTable.test.js',
+  './stacks/tests/stack.test.js',
+  './queues/tests/queue.test.js'
 ].forEach(function(test) {
   require(test);
 });
