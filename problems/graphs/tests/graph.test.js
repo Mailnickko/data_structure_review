240c7382cd26f95fc957a5b5493198c64d7c2eb7
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 01:07:02 -0700

    (test) Add tests for Graph

--- problems/graphs/tests/graph.test.js
+++ problems/graphs/tests/graph.test.js
@@ -12,19 +12,51 @@ describe('graph', () => {
     });
 
     it('should remove a node from the graph', () => {
-
+      const graph = new solution.Graph();
+      graph.addNode(1);
+      assert.equal(graph.contains(1), true);
+      graph.removeNode(1);
+      assert.equal(graph.contains(1), false);
+      // edges handled in removeEdges
     });
 
     it('should create edges between two nodes', () => {
-
+      const graph = new solution.Graph();
+      graph.addNode(1);
+      graph.addNode(2);
+      graph.addNode(3);
+      graph.addEdge(1,3);
+      assert.equal(graph.hasEdge(1,3), true);
+      assert.equal(graph.hasEdge(1,2), false);
     });
 
     it('should remove edges between two nodes', () => {
-
+      const graph = new solution.Graph();
+      graph.addNode(1);
+      graph.addNode(2);
+      graph.addNode(3);
+      graph.addEdge(1,3);
+      graph.addEdge(2,3);
+      assert.equal(graph.hasEdge(1,3), true);
+      assert.equal(graph.hasEdge(2,3), true);
+      graph.removeEdge(1,3);
+      graph.removeEdge(2,3);
+      assert.equal(graph.hasEdge(1,3), false);
+      assert.equal(graph.hasEdge(2,3), false);
     });
 
     it('should execute a callback on each node in graph', () => {
-
+      const graph = new solution.Graph();
+      graph.addNode(1);
+      graph.addNode(2);
+      graph.addNode(3);
+      assert.equal(graph.hasEdge(1,2), false);
+      assert.equal(graph.hasEdge(1,3), false);
+      graph.forEachNode( (node) => {
+        graph.addEdge(1, node);
+      });
+      assert.equal(graph.hasEdge(1,2), true);
+      assert.equal(graph.hasEdge(1,3), true);
     });
   });
 })
