baf6b9abb2302686fb41b5711bb62cf756bc5087
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 15:49:04 -0700

    (feat) Implement resizing in hash tables

--- problems/hashTables/prompts/hashTable.js
+++ problems/hashTables/prompts/hashTable.js
@@ -2,7 +2,7 @@ class HashTable {
 
   constructor() {
     this.storage = [];
-    this.limit = 4;
+    this.limit = 5;
     this.size = 0;
   }
 
@@ -15,7 +15,7 @@ class HashTable {
     for (var i = 0; i < bucket.length; i++) {
       tuple = bucket[i];
       if (tuple[0] === key) {
-        tuple[1] === value;
+        tuple[1] = value;
         exists = true;
       }
     }
@@ -23,6 +23,9 @@ class HashTable {
     if (!exists) {
       bucket.push([key, value]);
       this.size++;
+      if (this.size > this.limit * 0.75) {
+        this.resize(this.limit * 2);
+      }
     }
   }
 
@@ -41,13 +44,34 @@ class HashTable {
   }
 
   remove(key) {
-
+    let index = createHashKeyIndex(key, this.limit);
+    if (this.storage[index]) {
+      let bucket = this.storage[index];
+      let tuple;
+      for (var i = 0; i < bucket.length; i++) {
+        tuple = bucket[i];
+        if (tuple[0] === key) {
+          bucket.splice(i,1);
+          this.size--;
+          if (this.size < this.limit * 0.25) {
+            this.resize(this.limit / 2);
+          }
+        }
+      }
+    }
   }
 
   resize(limit) {
-
+    let newHash = new HashTable();
+    newHash.limit = limit;
+    this.storage.forEach((bucket) => {
+      bucket.forEach((tuple) => {
+        newHash.insert(tuple[0], tuple[1]);
+      });
+    });
+    this.storage = newHash.storage;
+    this.limit = limit;
   }
-
 }
 
 // Converts a given str into an integer between 0 and max-1
