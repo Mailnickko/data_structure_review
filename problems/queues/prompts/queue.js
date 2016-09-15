b41de93c079072a5b97ea9ddd873b71e7c3584a2
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 17:01:22 -0700

    (feat) Implement Queue methods

--- problems/queues/prompts/queue.js
+++ problems/queues/prompts/queue.js
@@ -1,19 +1,27 @@
-class Queue () {
+class Queue {
 
   constructor() {
-
+    this.firstKey = 0;
+    this.lastKey = 0;
+    this.storage = {};
   }
 
-  enqueue() {
-
+  enqueue(val) {
+    this.storage[this.lastKey] = val;
+    this.lastKey++;
   }
 
   dequeue() {
-
+    if (this.size()) {
+      var result = this.storage[this.firstKey];
+      delete this.storage[this.firstKey];
+      this.firstKey++;
+      return result;
+    }
   }
 
   size() {
-
+    return this.lastKey - this.firstKey;
   }
 }
 
