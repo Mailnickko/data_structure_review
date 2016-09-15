477e4dccd058db10bb24ea9cd763061b0be1903c
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 18:06:04 -0700

    (feat) Add Set and Tree test suites to manifest

--- problems/manifest.js
+++ problems/manifest.js
@@ -6,7 +6,9 @@
   './graphs/tests/graph.test.js',
   './hashTables/tests/hashTable.test.js',
   './stacks/tests/stack.test.js',
-  './queues/tests/queue.test.js'
+  './queues/tests/queue.test.js',
+  './sets/tests/set.test.js',
+  './trees/tests/tree.test.js'
 ].forEach(function(test) {
   require(test);
 });
