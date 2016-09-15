89a01f5de2433197f64ce8ddd4f3de2a45d142f4
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 23:35:23 -0700

    (test) Add tests for Tree

--- problems/trees/tests/tree.test.js
+++ problems/trees/tests/tree.test.js
@@ -9,23 +9,34 @@ describe('Trees', () => {
     tree = new solution.Tree();
   })
   describe('Tree', () => {
-    it('should have the methods addChild(), removeChild(), contains(), traverse()', () => {
+    it('should have the methods addChild(), removeFromParent(), contains(), traverse()', () => {
       expect(tree.addChild).to.be.a('function');
-      expect(tree.removeChild).to.be.a('function');
+      expect(tree.removeFromParent).to.be.a('function');
       expect(tree.contains).to.be.a('function');
       expect(tree.traverse).to.be.a('function');
     });
 
-    it('should true or false depending on if tree contains a value', () => {
-
+    it('should return true or false depending on if tree contains a value', () => {
+      tree.addChild('Hello');
+      expect(tree.contains('Hello')).to.be.equal(true);
+      expect(tree.contains('Bye')).to.be.equal(false);
     });
 
     it('should add children to a tree', () => {
-
+      expect(tree.contains(1)).to.be.equal(false);
+      tree.addChild(1);
+      expect(tree.contains(1)).to.be.equal(true);
+      expect(tree.contains(2)).to.be.equal(false);
     });
 
     it('should detect nested children', () => {
+      tree.addChild(1);
+      tree.addChild(2);
+      tree.children[0].addChild(3);
+      tree.children[0].addChild(4);
+      tree.children[0].children[0].addChild(7);
 
+      expect(tree.contains(7)).to.be.equal(true);
     });
   });
 });
