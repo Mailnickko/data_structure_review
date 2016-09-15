d3e96d6056c97c995fd1927deddfc1d79c778dac
Nicholas Ko <mailnickko@gmail.com>
2016-09-14 23:35:04 -0700

    (feat) Implement Tree methods

--- problems/trees/prompts/tree.js
+++ problems/trees/prompts/tree.js
@@ -1,23 +1,41 @@
 class Tree {
 
-  constructor() {
-
+  constructor(val) {
+    this.value = val;
+    this.parent = null;
+    this.children = [];
   }
 
   addChild(val) {
-
+    let newTree = new Tree(val);
+    this.children.push(newTree);
+    newTree.parent = this;
   }
 
   removeFromParent() {
-
+    let childIndex = this.parent.children.indexOf(this);
+    this.parent.children.splice(childIndex, 1);
+    this.parent = null;
   }
 
-  contains() {
-
+  contains(target) {
+    if (this.value === target) {
+      return true;
+    } else {
+      for (var i = 0; i < this.children.length; i++) {
+        if (this.children[i].contains(target)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
-  traverse() {
-
+  traverse(callback) {
+    callback(this.value);
+    this.children.forEach((child) => {
+      child.traverse(callback);
+    });
   }
 }
 
